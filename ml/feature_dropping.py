e5eefc7f13df5a888973026fd13a344863960970
Lior Sukman <lior.sukman@gmail.com>
2020-10-10 20:06:27 +0300

    feature dropping

---
+++ ml/feature_dropping.py
@@ -0,0 +1,70 @@
+import numpy as np
+from svm_gs_v2 import grid_search
+import time
+import NN_util
+from itertools import chain, combinations
+
+#indices of the features in the data
+indices = [[0, 1, 2, 3], [4, 5], [6, 7], [8, 9, 10], [11]]
+#name of each feature, corresponding to the indices list
+names = ['time lag', 'spatial dispersion', 'direction agreeableness', 'graph speeds', 'channels contrast']
+
+def powerset(s):
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s) + 1))
+
+def remove_features(keep, data):
+    clusters = []
+    for cluster in data:
+        clusters.append(cluster[:, keep])
+    return np.asarray(clusters)
+
+def print_results(results):
+    num_features = 0
+    for comb, acc in results:
+        if len(comb) != num_features:
+            print('----------------')
+            num_features = len(comb)
+            print('Results with %d feature(s)' % num_features)
+        message = 'Using the features: '
+        comb_fets = []
+        for ind in comb:
+            comb_fets.append(names[ind])
+        message += str(comb_fets)
+        message += ' accuracy is: '
+        print(message + '%.3f' % acc)
+            
+
+def feature_dropping():
+    per_train = 0.6
+    per_dev = 0.2
+    per_test = 0.2
+    NN_util.create_datasets(per_train, per_dev, per_test)
+    dataset_location = '../data_sets/clustersData_default' + '_' + str(per_train) + str(per_dev) + str(per_test) + '/'
+    train, dev, test = NN_util.get_dataset(dataset_location)
+
+    combinations = powerset(list(range(len(names))))
+
+    results = []
+
+    for comb in combinations:
+        inds = []
+        comb_fets = []
+        message = 'Used features are '
+        for i in comb:
+            inds += indices[i]
+            comb_fets.append(names[i])
+        message += str(comb_fets)
+        print(message)
+        inds.append(-1)
+        train_up = remove_features(inds, train)
+        dev_up = remove_features(inds, dev)
+        test_up = remove_features(inds, test)
+        _, accuracy = grid_search(train = train_up, dev = dev_up, test = test_up)
+        results.append((comb, accuracy))
+
+    print_results(results)
+                        
+
+if __name__ == "__main__":
+    feature_dropping()
+    
--- ml/svm_gs_v2.py
+++ ml/svm_gs_v2.py
@@ -8,7 +8,7 @@ import time
 import NN_util
 from VIS_heatmap import create_heatmap
 
-def evaluate_predictions(model, clusters):
+def evaluate_predictions(model, clusters, verbos = False):
     total = len(clusters)
     total_pyr = 0
     total_in = 0
@@ -30,23 +30,25 @@ def evaluate_predictions(model, clusters):
         correct_pyr += 1 if prediction == label and label == 1 else 0
         correct_in += 1 if prediction == label and label == 0 else 0
 
-    print('Number of correct classified clusters is %d, which is %.4f%s' % (correct_clusters, 100 * correct_clusters / total, '%'))
-    print('Number of correct classified chunks is %d, which is %.4f%s' % (correct_chunks, 100 * correct_chunks / total_chunks, '%'))
-    print('Test set consists of %d pyramidal cells and %d interneurons' % (total_pyr, total_in))
-    pyr_percent = float('nan') if total_pyr == 0 else 100 * correct_pyr / total_pyr
-    in_percent = float('nan') if total_in == 0 else 100 * correct_in / total_in
-    print('%.4f%s of pyrmidal cells classified correctly' % (pyr_percent, '%'))
-    print('%.4f%s of interneurons classified correctly' % (in_percent, '%'))
+    if verbos:
+        print('Number of correct classified clusters is %d, which is %.4f%s' % (correct_clusters, 100 * correct_clusters / total, '%'))
+        print('Number of correct classified chunks is %d, which is %.4f%s' % (correct_chunks, 100 * correct_chunks / total_chunks, '%'))
+        print('Test set consists of %d pyramidal cells and %d interneurons' % (total_pyr, total_in))
+        pyr_percent = float('nan') if total_pyr == 0 else 100 * correct_pyr / total_pyr
+        in_percent = float('nan') if total_in == 0 else 100 * correct_in / total_in
+        print('%.4f%s of pyrmidal cells classified correctly' % (pyr_percent, '%'))
+        print('%.4f%s of interneurons classified correctly' % (in_percent, '%'))
     return correct_clusters, correct_clusters / total
 
 
-def run():
-    per_train = 0.6
-    per_dev = 0.2
-    per_test = 0.2
-    NN_util.create_datasets(per_train, per_dev, per_test)
-    dataset_location = '../data_sets/clustersData_default' + '_' + str(per_train) + str(per_dev) + str(per_test) + '/'
-    train, dev, test = NN_util.get_dataset(dataset_location)
+def grid_search(verbos = False, train = None, dev = None, test = None):
+    if train is None or dev is None or test is None:
+        per_train = 0.6
+        per_dev = 0.2
+        per_test = 0.2
+        NN_util.create_datasets(per_train, per_dev, per_test)
+        dataset_location = '../data_sets/clustersData_default' + '_' + str(per_train) + str(per_dev) + str(per_test) + '/'
+        train, dev, test = NN_util.get_dataset(dataset_location)
 
     train_squeezed = NN_util.squeeze_clusters(train)
     dev_squeezed = NN_util.squeeze_clusters(dev)
@@ -57,7 +59,7 @@ def run():
     ps = PredefinedSplit(test_inds)         
 
     print()
-    parameters = {'C': np.logspace(1, 6, 12), 'gamma': np.logspace(-9, -2, 16)}
+    parameters = {'C': np.logspace(1, 5, 10), 'gamma': np.logspace(-8, -2, 14)}
     model = svm.SVC(kernel = 'rbf', class_weight = 'balanced')
     clf = GridSearchCV(model, parameters, cv = ps)
     print('Starting grid search...')
@@ -67,14 +69,15 @@ def run():
     print('Grid search completed in %.2f seconds, best parameters are:' % (end - start)) 
     print(clf.best_params_)
 
-    scores = clf.cv_results_['mean_test_score']
-    cs = [round(v, 3) for v in np.logspace(1, 6, 12)]
-    gammas = [round(v, 9) for v in np.logspace(-9, -2, 16)]
-    create_heatmap(gammas, cs, 'Gamma', 'C', 'SVM Grid Search', scores.reshape((len(cs), len(gammas))))
+    if verbos:
+        scores = clf.cv_results_['mean_test_score']
+        cs = [round(v, 3) for v in np.logspace(1, 6, 12)]
+        gammas = [round(v, 9) for v in np.logspace(-9, -2, 16)]
+        create_heatmap(gammas, cs, 'Gamma', 'C', 'SVM Grid Search', scores.reshape((len(cs), len(gammas))))
 
     print()
     print('Starting evaluation on test set...')
-    evaluate_predictions(clf, test)
+    return evaluate_predictions(clf, test, verbos)
 
 if __name__ == "__main__":
-    run()
+    grid_search(verbos = True)
