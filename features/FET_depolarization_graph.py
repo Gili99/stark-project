9a0aeb1c27407854340de5cf31cd7c211153188f
Gil <gilnd99@gmail.com>
2020-10-24 17:50:52 +0300

    Added documentation for the features, removed magnitude distribution and trough-2-peak

--- features/FET_depolarization_graph.py
+++ features/FET_depolarization_graph.py
@@ -2,6 +2,9 @@ import numpy as np
 import math
 
 class Graph(object):
+    """
+    An object used to represent a graph
+    """
     def __init__(self, startNodes, endNodes, graphMatrix):
         self.startNodes = startNodes
         self.endNodes = endNodes
@@ -10,21 +13,19 @@ class Graph(object):
         self.edges = self._getAllEdges()
 
     def flip_graph(self):
+        """
+        Flips the values of the weights in the graph, i.e Positive weights will become negative
+        """
         self.edges = [(e[0], e[1], e[2] * -1) for e in self.edges]
         self.reversed = not self.reversed
 
     def averageWeight(self):
+        """
+        returns:
+        The average weight in the graph
+        """
         total = 0
         counter = 0
-        """
-        for i in range(256):
-            for j in range(256):
-                if self.graphMatrix[i, j] != -1:
-                    total += self.graphMatrix[i, j]
-                    counter +=1
-        if counter == 0:
-            return 0
-            """
         total = sum(e[2] for e in self.edges)
         counter = len(self.edges)
 
@@ -34,7 +35,15 @@ class Graph(object):
         return (total / counter)
 
     def _getAllEdges(self):
+        """
+        returns:
+        a list of all edges in the graph. each edge is represented using a tupple:
+            (from node, to node, weight of edge)
+        """
         edges = []
+
+        # Check to see if the grpah is currently reversed and determine what is the value that will
+        # be used to indicate that an edge doesn't exist
         if self.reversed:
             not_exist = 1
         else:
@@ -47,6 +56,15 @@ class Graph(object):
         return edges
 
     def _belmanFord(self, srcNode):
+        """
+        An implemetation of the Belman Ford algorithm to find shortest distances in a graph
+
+        inputs:
+        srcNode: The src node from which the search will begin
+
+        returns:
+        A list of size |nodes| containing the minimal distance from the source node to each other node
+        """
         sizeV = 256 # this is |V|
         dists = [float('inf') for i in range(sizeV)]
         dists[srcNode] = 0
@@ -64,6 +82,13 @@ class Graph(object):
         return dists
 
     def _findMinimumDistToEndNodes(self, dists):
+        """
+        inputs:
+        dists: the output of the Belman Ford algorithm
+
+        returns:
+        The minimum distnace to any of the end nodes of the graph
+        """
         minimum = float('inf')
         for endNode in self.endNodes:
             if dists[endNode] < minimum:
@@ -71,6 +96,10 @@ class Graph(object):
         return minimum
 
     def shortestDistanceFromSrcToEnd(self):
+        """
+        returns:
+        The shortest distnace from any source node to any end node
+        """
         totalMinDist = float('inf')
         for srcNode in self.startNodes:
             shortDists = self._belmanFord(srcNode)
@@ -80,19 +109,43 @@ class Graph(object):
         return totalMinDist
     
     def longestDistanceFromSrcToEnd(self):
+        """
+        returns:
+        The longest distnace from any source node to any end node
+        """
         self.flip_graph()
         longest = self.shortestDistanceFromSrcToEnd()
         self.flip_graph()
         return longest * -1
 
 class DepolarizationGraph(object):
+    """
+    This feature tries to estimate the way the signal traverses between the different channels. This traversal is modeled
+    into a graph, where each node indicates a channel in a certain time, and each edge represents the speed in which the 
+    signal travels between the two channels that comprise it.
+    """
     def __init__(self):
         self.name = 'depolarization graph'
 
     def euclideanDist(self, pointA, pointB):
+        """
+        inputs:
+        pointA: (x,y) tupple representing a point in 2D space
+        pointB: (x,y) tupple representing a point in 2D space
+
+        returns:
+        The euclidean distance between the points
+        """
         return math.sqrt((pointA[0] - pointB[0]) ** 2 + (pointA[1] - pointB[1]) ** 2)
 
     def calculateDistancesMatrix(self, coordinates):
+        """
+        inputs:
+        coordinates: a list of (x, y) tupples representing the coordinates of different channels
+
+        returns:
+        A 2D matrix in which a cell (i, j) contains the distance from coordiane i to coordinate j
+        """
         distances = np.zeros((8, 8))
         for i in range(8):
             for j in range(8):
@@ -101,6 +154,13 @@ class DepolarizationGraph(object):
         return distances
 
     def get_indices_with_one(self, arr):
+        """
+        inputs:
+        arr: a 2 dimensional matrix
+
+        returns:
+        the number of cells in the matrix that contain the value 1
+        """
         lst = []
         for i in range(len(arr)):
             if arr[i] == 1:
@@ -108,6 +168,14 @@ class DepolarizationGraph(object):
         return lst
     
     def calculateFeature(self, spikeList):
+        """
+        inputs:
+        spikeList: A list of Spike object that the feature will be calculated upon.
+
+        returns:
+        A matrix in which entry (i, j) refers to the j metric of Spike number i.
+        """
+        # Determine the (x,y) coordinates of the 8 different channels and calculate the distances matrix
         coordinates = [(0, 0), (-9, 20), (8, 40), (-13, 60), (12, 80), (-17, 100), (16, 120), (-21, 140)]
         dists = self.calculateDistancesMatrix(coordinates)
         result = np.zeros((len(spikeList), 3))
@@ -115,7 +183,7 @@ class DepolarizationGraph(object):
         for index, spike in enumerate(spikeList):
             arr = spike.data
             minVal = arr.min()
-            threshold = 0.3 * minVal
+            threshold = 0.3 * minVal # Setting the threshold to be 0.3 the size of max depolarization
 
             # Detrmine where the maximum depolarization resides wrt each channel (that surpasses the threshold)
             depolarizationStatus = np.zeros((8, 32))
@@ -146,10 +214,11 @@ class DepolarizationGraph(object):
                         velocity = dists[fromNode, toNode] / (toTimestamp - fromTimestamp)
                         graphMatrix[fromNode + fromTimestamp * 8][toNode + toTimestamp * 8] = velocity
 
-            initialTime = gTemp[0][0]
-            endTime = gTemp[len(gTemp)-1][0]
-            startNodes = [node + initialTime * 8 for node in startNodes]
-            endNodes = [node + endTime * 8 for node in endNodes]
+            # Build the actual graph based on the data that was collected in the previous stage
+            initialTime = gTemp[0][0] # Time of first channel reaching deplarization
+            endTime = gTemp[len(gTemp)-1][0] # Time of last chaneel reaching depolarization            
+            startNodes = [node + initialTime * 8 for node in startNodes] # The first nodes that reached depolarization
+            endNodes = [node + endTime * 8 for node in endNodes] # The last nodes that reached depolarization
             graph = Graph(startNodes, endNodes, graphMatrix)
             
             # Calculate features from the graph
@@ -157,26 +226,12 @@ class DepolarizationGraph(object):
             result[index, 1] = graph.shortestDistanceFromSrcToEnd()
             result[index, 2] = graph.longestDistanceFromSrcToEnd()
 
-            # THIS IS A HACK! DELETE WHEN FINISHED
-            """import matplotlib.pyplot as plt
-            if round(result[index, 0], 2) == 18.45:
-                for i in range(8):
-                    plt.plot([i for i in range(32)], arr[i, :], label = 'channel ' + str(i+1))
-                
-                title = "graph_avg_speed=" + str(round(result[index, 0], 2)) + ", graph_slowest_path=" + str(round(result[index, 1], 2)) + ", graph_fastest_speed=" + str(round(result[index, 2], 2))
-                plt.title(title)
-                plt.axes().get_yaxis().set_visible(False)
-                maxDep = arr.min(axis=0).min()
-                plt.axhline(y=maxDep * 0.3, c='black', linestyle='--')
-                plt.legend(loc="lower right")
-                plt.show()
-
-                print(depolarizationStatus)
-                print(graph._getAllEdges())"""
-
         return result
 
     def get_headers(self):
+        """
+        Returns a list of titles of the different metrics
+        """
         return ["graph_avg_speed", "graph_slowest_path", "graph_fastest_path"]
 
 if __name__ == "__main__":
--- features/FET_time_lag.py
+++ features/FET_time_lag.py
@@ -1,25 +1,55 @@
 import numpy as np
 from clusters import Spike
 
+# There are two options of reduction for the da vector:
+# ss - sum of squares
+# sa - sum of absolutes
 reduction_types = ['ss', 'sa']
 
 class Time_Lag_Feature(object):
+    """
+    This feature calculates the time difference between the main channel and all other channels in terms of
+    maximal depolarization, and the following after hyperpolarization.
+    The feature only takes into consideration channels that have crossed a ceratin threshold, dtermined by the 
+    maximal depolarization of the main channel.
+    """
     def __init__(self, type_dep = 'ss', type_hyp = 'ss', ratio = 0.25):
         assert type_dep in reduction_types and type_hyp in reduction_types
         
+        # Reduction types
         self.type_dep = type_dep
         self.type_hyp = type_hyp
 
-        self.ratio = ratio
+        self.ratio = ratio # Indicates the percantage of the maximum depolarization that will be considered as a threshold
 
         self.name = 'time lag feature'
 
     def calculateFeature(self, spikeList):
+        """
+        inputs:
+        spikeList: A list of Spike object that the feature will be calculated upon.
+
+        returns:
+        A matrix in which entry (i, j) refers to the j metric of Spike number i.
+        """
         result = [self.calc_feature_spike(spike.get_data()) for spike in spikeList]
         result = np.asarray(result)
         return result
 
     def calc_feature_spike(self, spike):
+        """
+        inputs:
+        spike: the spike to be processed; it is a matrix with the dimensions of (8, 32)
+
+        The function calculates different time lag features of the spike
+
+        returns:
+        a list containing the following values:
+            -dep_red: the reduction of the depolarization vactor (i.e the vector that indicates the time difference of maximal depolarization between each channel and the main channel)
+            -dep_sd: the standard deviation of the depolarization vector
+            -hyp_red: the reduction of the hyperpolarization vector
+            -hyp_sd: the standard deviation of the hyperpolarization vector
+        """
         # remove channels with lower depolarization than required
         deps = np.min(spike, axis = 1) # max depolarization of each channel
         max_dep = np.min(deps)
@@ -66,6 +96,9 @@ class Time_Lag_Feature(object):
         return [dep_red, dep_sd, hyp_red, hyp_sd]
 
     def get_headers(self):
+        """
+        Returns a list of titles of the different metrics
+        """
         return ['dep_red', 'dep_sd', 'hyp_red', 'hyp_sd']
 
     
